c602ad4032dbd5b7abbdd6188445c914c34fb946
ChandonPierre <cpierre@coreweave.com>
2021-09-02 20:06:19 -0400

    Add cross-region clone via DataVolume

--- virtual-server/pvc-clone.sh
+++ virtual-server/pvc-clone.sh
@@ -2,14 +2,14 @@
 # Clone the disk PVC for a given VM instance to a new PVC.
 set -e -o pipefail -u
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <source vmi> <destination pvc name>"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <source vmi> <destination pvc name> <ord1|ewr1|las1>"
     exit 1
 fi
 
 SRC="$1"
 DST="$2"
-
+DSTREGION="$3"
 
 get_field() {
     kubectl get $1 $2 -o=jsonpath='{'"$3"'}'
@@ -55,6 +55,10 @@ fi
 
 DST_PVC="${DST}-$(date '+%Y%m%d')-block-${REGION}"
 
+if [ "$REGION" == "$DSTREGION" ]
+
+then
+
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: PersistentVolumeClaim
@@ -79,6 +83,47 @@ while [ $(get_field pvc $DST_PVC ".status.phase") != "Bound" ]; do
     echo -n "."
 done
 echo " done."
+
+else
+
+NS=$(kubectl config view --minify --output 'jsonpath={..namespace}')
+DST_PVC_CLASS=$(echo ${SRC_PVC_CLASS} | sed s/....$/${DSTREGION}/)
+DST_PVC="${DST}-$(date '+%Y%m%d')-block-${DSTREGION}"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: cdi.kubevirt.io/v1beta1
+kind: DataVolume
+metadata:
+  annotations:
+  labels:
+  name: ${DST_PVC}
+  namespace: ${NS}
+spec:
+  pvc:
+    accessModes:
+    - ReadWriteOnce
+    resources:
+      requests:
+        storage: ${SRC_PVC_SIZE}
+    storageClassName: ${DST_PVC_CLASS}
+    volumeMode: Block
+  source:
+    pvc:
+      name: ${SRC_PVC}
+      namespace: ${NS}
+EOF
+
+  
+
+echo -n "Waiting for DV $DST_PVC to complete clone..."
+while [ $(kubectl get dv ${DST_PVC} --output 'jsonpath={.status.phase}') != "Succeeded" ]; do
+    sleep 1
+    echo -n "."
+done
+
+echo " done."
+
+fi
 
 echo "Clone of $SRC_PVC to $DST_PVC is complete."
 echo "Source VM instance $SRC can be started up or destroyed."
