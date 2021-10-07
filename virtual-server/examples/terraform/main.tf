e87c4dcbde27a6a0cecc2b7904d5d32f9ba3a22d
Cesar Mesones <cmesones@coreweave.com>
2021-10-07 14:09:55 -0400

    squash: remove deprecated provider

--- virtual-server/examples/terraform/main.tf
+++ virtual-server/examples/terraform/main.tf
@@ -1,11 +1,6 @@
 provider "kubernetes" {
   config_path = var.kubeconfig_path
 }
-
-provider "kubernetes-alpha" {
-  config_path = var.kubeconfig_path
-}
-
 resource "random_string" "vs_generate_password" {
   count            = var.vs_generate_password ? 1 : 0
   length           = 16
