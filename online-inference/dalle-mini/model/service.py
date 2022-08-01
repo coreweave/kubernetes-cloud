5c6c1178c5ef5cc0102f5e4f81c7a9b3f63e12fc
Tyler Weldon <tylerweldon94@gmail.com>
2022-08-01 13:10:10 -0400

    tabs/spaces fix; b64 encode image for json payload

--- online-inference/dalle-mini/model/service.py
+++ online-inference/dalle-mini/model/service.py
@@ -2,6 +2,8 @@ import kserve
 import logging
 import os
 import random
+import base64
+from io import BytesIO
 from typing import Dict
 
 import jax
@@ -17,8 +19,8 @@ from transformers import CLIPProcessor, FlaxCLIPModel
 from vqgan_jax.modeling_flax_vqgan import VQModel
 
 options = {
-    'MODEL_NAME': os.getenv('MODEL_NAME', default="dalle-mini/dalle-mini"),
-    'MODEL_CACHE': os.getenv('MODEL_CACHE', default="/model-cache")
+	'MODEL_NAME': os.getenv('MODEL_NAME', default="dalle-mini/dalle-mini"),
+	'MODEL_CACHE': os.getenv('MODEL_CACHE', default="/model-cache")
 }
 
 model_options = {
@@ -47,103 +49,108 @@ seed = random.randint(0, 2**32 - 1)
 key = jax.random.PRNGKey(seed)
 
 class Model(kserve.Model):
-    def __init__(self, name: str):
-        super().__init__(name)
-        self.name = name
-        self.ready = False
-        self.model = None
-        self.model_params = None
-        self.vqgan = None
-        self.vqgan_params = None
-        self.processor = None
-        self.generator = None
-        self.model_name = options['MODEL_NAME']
+	def __init__(self, name: str):
+		super().__init__(name)
+		self.name = name
+		self.ready = False
+		self.model = None
+		self.model_params = None
+		self.vqgan = None
+		self.vqgan_params = None
+		self.processor = None
+		self.generator = None
+		self.model_name = options['MODEL_NAME']
 
-    def load(self):
-        self.model, model_params = DalleBart.from_pretrained(
-		    options['MODEL_NAME'],
-		    revision=None, 
-		    dtype=jnp.float16, 
-		    _do_init=False,
-		    #local_files_only=True
+	def load(self):
+		self.model, model_params = DalleBart.from_pretrained(
+			options['MODEL_NAME'],
+			revision=None, 
+			dtype=jnp.float16, 
+			_do_init=False,
+			#local_files_only=True
 		)
-        self.model_params = replicate(model_params)
-        self.vqgan, vqgan_params = VQModel.from_pretrained(
-		    VQGAN_REPO,
-            revision=VQGAN_COMMIT_ID,
-            _do_init=False
+		self.model_params = replicate(model_params)
+		self.vqgan, vqgan_params = VQModel.from_pretrained(
+			VQGAN_REPO,
+			revision=VQGAN_COMMIT_ID,
+			_do_init=False
 		)
-        self.vqgan_params = replicate(vqgan_params)
-        self.processor = DalleBartProcessor.from_pretrained(options['MODEL_NAME'], revision=None)
-        self.ready=True
-    
-    @partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(0, 4, 5, 6, 7))
-    def p_generate(
-	    self, tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
-	):
-        return self.model.generate(
-		        **tokenized_prompt,
-		        prng_key=key,
-		        params=params,
-		        top_k=top_k,
-		        top_p=top_p,
-		        temperature=temperature,
-		        condition_scale=condition_scale,
-		    )
-
-    @partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(0))
-    def p_decode(self, indices, params):
-        return self.vqgan.decode_code(indices, params=params)
-
-    def configure_request(self, request: Dict, generation_params) -> Dict:
-        parameters = request['parameters']
-        for k, pv in parameters.items():
-            pk = k.upper()
-            if pk in generation_params:
-                logger.debug(f'Parameter {pk} changed from {request_params[pk]} to {pv}')
-                request_params[pk] = pv
-
-        return generation_params
-
-    def predict(self, request: Dict) -> Dict:
-        seed = random.randint(0, 2**32 - 1)
-        key = jax.random.PRNGKey(seed)
-
-        tokenized_prompt = self.processor([request['prompt']])
-        tokenized_prompt = replicate(tokenized_prompt)
-
-        #images = []
-        for i in trange(max(model_options['N_PREDICTIONS'] // jax.device_count(), 1)):
-            key, subkey = jax.random.split(key)
-            encoded_images = self.p_generate(
-		        tokenized_prompt,
-		        shard_prng_key(subkey),
-		        self.model_params,
-		        TOP_K,
-		        TOP_P,
-		        TEMPERATURE,
-		        CONDITION_SCALE,
-		    )
-
-            encoded_images = encoded_images.sequences[..., 1:]
-            decoded_images = self.p_decode(encoded_images, self.vqgan_params)
-            decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+		self.vqgan_params = replicate(vqgan_params)
+		self.processor = DalleBartProcessor.from_pretrained(options['MODEL_NAME'], revision=None)
+		self.ready=True
 	
-            for decoded_img in decoded_images:
-	            img = Image.fromarray(np.asarray(decoded_img * 255, dtype=np.uint8))
-	            #images.append(img)
+	@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(0, 4, 5, 6, 7))
+	def p_generate(
+		self, tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
+	):
+		return self.model.generate(
+				**tokenized_prompt,
+				prng_key=key,
+				params=params,
+				top_k=top_k,
+				top_p=top_p,
+				temperature=temperature,
+				condition_scale=condition_scale,
+			)
 
-        return {'image': img}
+	@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(0))
+	def p_decode(self, indices, params):
+		return self.vqgan.decode_code(indices, params=params)
 
-    @staticmethod
-    def is_ready():
-        pass
+	def configure_request(self, request: Dict, generation_params) -> Dict:
+		parameters = request['parameters']
+		for k, pv in parameters.items():
+			pk = k.upper()
+			if pk in generation_params:
+				logger.debug(f'Parameter {pk} changed from {request_params[pk]} to {pv}')
+				request_params[pk] = pv
+
+		return generation_params
+
+	def predict(self, request: Dict) -> Dict:
+		seed = random.randint(0, 2**32 - 1)
+		key = jax.random.PRNGKey(seed)
+
+		tokenized_prompt = self.processor([request['prompt']])
+		tokenized_prompt = replicate(tokenized_prompt)
+
+		#images = []
+		for i in trange(max(model_options['N_PREDICTIONS'] // jax.device_count(), 1)):
+			key, subkey = jax.random.split(key)
+			encoded_images = self.p_generate(
+				tokenized_prompt,
+				shard_prng_key(subkey),
+				self.model_params,
+				TOP_K,
+				TOP_P,
+				TEMPERATURE,
+				CONDITION_SCALE,
+			)
+
+			encoded_images = encoded_images.sequences[..., 1:]
+			decoded_images = self.p_decode(encoded_images, self.vqgan_params)
+			decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+	
+			for decoded_img in decoded_images:
+				img = Image.fromarray(np.asarray(decoded_img * 255, dtype=np.uint8))
+				img_file = BytesIO()
+				img.save(img_file, format="PNG")
+				img_bytes = img_file.getvalue()
+				payload = base64.b64encode(img_bytes)
+				payload = str(payload, 'utf-8')
+				#images.append(img)
+
+		return {'image': payload}
+
+	@staticmethod
+	def is_ready():
+		pass
 
 if __name__ == '__main__':
-    #Model.is_ready()
-    model = Model(options['MODEL_NAME'])
-    logger.info(f'Loading model {options["MODEL_NAME"]}')
-    model.load()
-    logger.info(f'Model {options["MODEL_NAME"]} loaded')
-    logger.info(f'Starting Model Server')
-    kserve.ModelServer().start([model])
+	#Model.is_ready()
+	model = Model(options['MODEL_NAME'])
+	logger.info(f'Loading model {options["MODEL_NAME"]}')
+	model.load()
+	logger.info(f'Model {options["MODEL_NAME"]} loaded')
+	logger.info(f'Starting Model Server')
+	kserve.ModelServer().start([model])
