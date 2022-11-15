901ef19c87b4b3c98fa272344146ca41e39cf1b1
rtalaricw <rtalari@coreweave.com>
2022-11-15 08:24:55 -0800

    feat(grpc): GRPC Streaming Client Example for Fastertransformer

--- online-inference/fastertransformer/client/example.py
+++ online-inference/fastertransformer/client/example.py
@@ -1,9 +1,14 @@
 import json
 import torch
+import time
+import google.protobuf.json_format
+import multiprocessing as mp
 import numpy as np
 import tritonclient.grpc as grpcclient
 import tritonclient.http as httpclient
 
+from tritonclient.grpc.service_pb2 import ModelInferResponse
+from functools import partial
 from argparse import ArgumentParser
 from collections.abc import Mapping
 from tritonclient.utils import np_to_triton_dtype
@@ -16,13 +21,8 @@ import gpt_bpe.gpt_token_encoder as encoder
 
 merge_file = "/workspace/gpt_bpe/gpt2-merges.txt"
 vocab_file = "/workspace/gpt_bpe/gpt2-vocab.json"
+
 enc = encoder.get_encoder(vocab_file, merge_file)
-
-# HFTokenizer
-from hf_tokenizer.hf_tokenize import HFTokenizer 
-
-hf_encoder_vocab_file = "/workspace/hf_tokenizer/20B_tokenizer.json"
-hf_enc = HFTokenizer(vocab_file=hf_encoder_vocab_file)
 
 def deep_update(source, overrides):
     """
@@ -37,24 +37,18 @@ def deep_update(source, overrides):
             source[key] = overrides[key]
     return source
 
-def encode_data(model, text):
-    if model == "gptj":
-        all_ids = [torch.IntTensor(enc.encode(text))]
-    elif model == "gpt-neox":
-        all_ids = [torch.IntTensor(hf_enc.tokenize(text))]
+def encode_data(text="Hello, I am Jane. Nice to meet you!"):
+    all_ids = [torch.IntTensor(enc.encode(text))]
     return all_ids[0].numpy()
 
 
-def decode_data(array, model):
-    if model == "gptj":
-        sentence = enc.decode(array)
-    elif model == "gpt-neox":
-        sentence = hf_enc.detokenize(array)
+def decode_data(array):
+    sentence = enc.decode(array)
     return sentence
 
 def generate_parameters(args):
     DEFAULT_CONFIG = {
-        'protocol': 'http',
+        'protocol': args.protocol,
         'url': f'{args.url}:80',
         'model_name': 'fastertransformer',
         'verbose': False,
@@ -67,11 +61,11 @@ def generate_parameters(args):
 
     deep_update(params, file_params)
 
-    input_lengths = len(encode_data(text=args.prompt, model=args.model))
+    input_lengths = len(encode_data(text=args.prompt))
 
     for index, value in enumerate(params['request']):
         if value['name'] == 'input_ids':
-            value['data'] = [encode_data(model=args.model, text=args.prompt)] 
+            value['data'] = [encode_data(text=args.prompt)] 
         if value['name'] == 'input_lengths':
             value['data'] = [[input_lengths]]
         params['request'][index] = {
@@ -79,7 +73,6 @@ def generate_parameters(args):
             'data': np.array(value['data'], dtype=value['dtype']),
         }
     
-
     return params['config'], params['request']
 
 
@@ -88,10 +81,60 @@ def prepare_tensor(client, name, input):
     t.set_data_from_numpy(input)
     return t
 
+def stream_consumer(queue):
+    print(f"queue: {queue}")
+    start_time = None
+    while True:
+        result = queue.get()
+        if result is None:
+            break
 
-def main(config, request):
+        if isinstance(result, float):
+            start_time = result
+            continue
+
+        message = ModelInferResponse()
+        google.protobuf.json_format.Parse(json.dumps(result), message)
+        result = grpcclient.InferResult(message)
+
+        print(f"result GRPC : {result}")
+        idx = result.as_numpy("sequence_length")[0, 0]
+        tokens = result.as_numpy("output_ids")[0, 0, :idx]
+        prob = result.as_numpy("cum_log_probs")[0, 0]
+        print("[After {:.2f}s] Partial result (probability {:.2e}):\n{}\n".format(
+            time.perf_counter() - start_time, np.exp(prob), tokens))
+
+        for output in result.get_response().outputs:
+            if output.name == "output_ids":
+                print(f"Ouput: {decode_data(list(result.as_numpy(output.name)[0][0]))}")
+
+def stream_callback(queue, result, error):
+    if error:
+        queue.put(error)
+    else:
+        queue.put(result.get_response(as_json=True))
+
+
+def main_grpc(config, request):
+    result_queue = mp.Queue()
+
+    consumer = mp.Process(target=stream_consumer, args=(result_queue,))
+    consumer.start()
+
+    with grpcclient.InferenceServerClient(config['url'], verbose=config["verbose"]) as cl:
+        payload = [prepare_tensor(grpcclient, field['name'], field['data'])
+            for field in request]
+        
+        print(f"payload : {payload}")
+        cl.start_stream(callback=partial(stream_callback, result_queue))
+        result_queue.put(time.perf_counter())
+        cl.async_stream_infer(config['model_name'], payload)
+    result_queue.put(None)
+    consumer.join()
+
+def main_http(config, request):
     client_type = httpclient if config['protocol'] == 'http' else grpcclient
-    with client_type.InferenceServerClient(config['url'], verbose=config['verbose'], concurrency=1, connection_timeout=100.0, network_timeout=100.0,) as cl:
+    with client_type.InferenceServerClient(config['url'], verbose=config['verbose']) as cl:
         payload = [prepare_tensor(client_type, field['name'], field['data'])
             for field in request]
 
@@ -99,13 +142,13 @@ def main(config, request):
 
     for output in result.get_response()['outputs']:
         if output['name'] == "output_ids":
-            print(f"Ouput: {decode_data(list(result.as_numpy(output['name'])[0][0]), model=args.model)}")
+            print(f"Ouput: {decode_data(list(result.as_numpy(output['name'])[0][0]))}")
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--url", help="InferenceService URL for Fastertransfomer", required=True)
     parser.add_argument("--prompt", help="Prompt to generate based of.", required=True)
-    parser.add_argument("--model", help="gptj or gpt-neox", required=True)
+    parser.add_argument("--protocol", help="Prompt to generate based of.", required=True)
     
     args = parser.parse_args()
 
@@ -114,4 +157,7 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     config, request = generate_parameters(args)
-    main(config, request)
+    if args.protocol == "http":
+        main_http(config, request)
+    elif args.protocol == "grpc":
+        main_grpc(config, request)
