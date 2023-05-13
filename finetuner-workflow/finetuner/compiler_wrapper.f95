e7f7a95accdae07deedd8802774def7d2b730eba
Eta <esyra@coreweave.com>
2023-05-12 19:24:03 -0500

    fix(finetuner): Pre-compile dependencies without using `-march=native`
    DeepSpeed's build process forces in the compile flag `-march=native` and
    potentially the preprocessor definition `-D__AVX512__` depending on
    the system the Dockerfile is built on. This can lead to errors if
    it is built on a system with too new of a CPU, since it might include
    instructions not available on nodes' CPUs. This replaces occurrences
    of `-march=native` with `-march=skylake` to make the builds more
    reproducible, and replaces the preprocessor definition `-D__AVX512__`
    with `-D__AVX256__` since the CPUS on most nodes don't support AVX512
    instructions yet.

---
+++ finetuner-workflow/finetuner/compiler_wrapper.f95
@@ -0,0 +1,74 @@
+PROGRAM compiler_wrapper
+    ! Wraps GCC invocations, replacing -D__AVX512__ preprocessor definitions
+    ! with -D__AVX256__, and -march=native with -march=skylake,
+    ! for better reproducibility and compatibility.
+    IMPLICIT NONE
+    INTEGER :: i, exitcode = 0, full_length = 0, truncated = 0
+    CHARACTER(len=:), ALLOCATABLE :: arg, command
+    ALLOCATE(CHARACTER(len=128) :: arg)
+    command = "gcc"
+
+    DO i = 1, COMMAND_ARGUMENT_COUNT()
+        DO
+            CALL GET_COMMAND_ARGUMENT(i, arg, full_length, truncated)
+            IF (truncated == 0) THEN
+                EXIT
+            ELSEIF (truncated == -1) THEN
+                DEALLOCATE(arg)
+                ALLOCATE(CHARACTER(len=full_length) :: arg)
+            ELSE
+                CALL EXIT(95)
+            END IF
+        END DO
+        IF (arg == "-march=native") THEN
+            command = command // " '-march=skylake'"
+        ELSEIF (arg == "-D__AVX512__") THEN
+            command = command // " '-D__AVX256__'"
+        ELSE
+            command = command // shell_escaped(arg)
+        END IF
+    END DO
+    CALL SYSTEM(command, exitcode)
+    IF (exitcode > 255) THEN
+        exitcode = MAX(IAND(exitcode, 255), 1)
+    ENDIF
+    CALL EXIT(exitcode)
+
+
+    CONTAINS
+        FUNCTION shell_escaped(str) result(out)
+            ! Turns [str] into [ 'str'] and replaces all
+            ! internal ['] characters with ['"'"']
+            IMPLICIT NONE
+            CHARACTER(len=:), ALLOCATABLE, INTENT(IN) :: str
+            CHARACTER(len=:), ALLOCATABLE :: out
+            INTEGER :: i, out_i, old_len, out_len
+
+            old_len = LEN_TRIM(str)
+            ! Figure out the new length to allocate by scanning `str`.
+            ! This always needs to add at least [ '] at the beginning
+            ! and ['] at the end, so the length increases by at least 3.
+            out_len = old_len + 3
+            DO i = 1, old_len
+                IF (str(i:i) == "'") THEN
+                    out_len = out_len + 4
+                END IF
+            END DO
+            ALLOCATE(CHARACTER(len=out_len) :: out)
+
+            out(1:2) = " '"
+            out_i = 3
+            DO i = 1, old_len
+                IF (str(i:i) == "'") THEN
+                    ! Escape internal single-quotes
+                    out(out_i:out_i + 4) = '''"''"'''
+                    out_i = out_i + 5
+                ELSE
+                    ! No escaping needed
+                    out(out_i:out_i) = str(i:i)
+                    out_i = out_i + 1
+                END IF
+            END DO
+            out(out_i:out_i) = "'"
+        END FUNCTION
+END PROGRAM
