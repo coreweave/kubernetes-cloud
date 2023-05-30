502177304cd63a047000a018042f4b05f5c079fb
Eta <esyra@coreweave.com>
2023-05-30 18:22:25 -0500

    feat(tensorizer-isvc): Add more logging control for benchmarks

--- online-inference/tensorizer-isvc/benchmark/load_test_async.py
+++ online-inference/tensorizer-isvc/benchmark/load_test_async.py
@@ -1,9 +1,26 @@
 import asyncio
+import logging
 import random
+import sys
 import time
 from argparse import ArgumentParser
 
 import aiohttp
+
+logger = logging.getLogger(__name__)
+inputs = [
+    "Hello, how are you?",
+    "What up dig dog?",
+    "You are a killer!",
+    "Live a good life",
+    "Life is great",
+    "Chilling on a roof",
+    "Love you",
+    "Mox is cute",
+    "You are my enemy",
+    "Change is required",
+    "Love the life",
+]
 
 
 def timed(func):
@@ -31,40 +48,54 @@ async def async_aiohttp_get_all(urls):
 
         async def fetch(url):
             async with session.get(url) as response:
-                print(response.status)
+                log_level = logging.INFO if response.ok else logging.WARNING
+                logger.log(
+                    log_level, f"Status {response.status} response from {url}"
+                )
                 return await response.text()
 
         return await asyncio.gather(*map(fetch, urls))
 
 
+def benchmark(base_url):
+    urls = [f"{base_url}/predict/{random.choice(inputs)}" for _ in range(100)]
+    timed(asyncio.run)(async_aiohttp_get_all(urls))
+
+
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--url", help="InferenceService URL", required=True)
+    parser.set_defaults(log_level=logging.WARNING)
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Show more logs",
+        dest="log_level",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "--quiet",
+        "-q",
+        help="Show fewer logs",
+        dest="log_level",
+        action="store_const",
+        const=logging.ERROR,
+    )
 
-    args = parser.parse_args()
-
-    return args
+    return parser.parse_args()
 
 
-inputs = [
-    "Hello, how are you?",
-    "What up dig dog?",
-    "You are a killer!",
-    "Live a good life",
-    "Life is great",
-    "Chilling on a roof",
-    "Love you",
-    "Mox is cute",
-    "You are my enemy",
-    "Change is required",
-    "Love the life",
-]
+def setup_logger(level):
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.propagate = False
+    logger.setLevel(level)
 
 
 def main():
     args = parse_args()
-    urls = [f"{args.url}/predict/{random.choice(inputs)}" for _ in range(100)]
-    timed(asyncio.run)(async_aiohttp_get_all(urls))
+    setup_logger(args.log_level)
+    benchmark(args.url)
 
 
 if __name__ == "__main__":
--- online-inference/tensorizer-isvc/benchmark/load_test_sync.py
+++ online-inference/tensorizer-isvc/benchmark/load_test_sync.py
@@ -1,9 +1,12 @@
+import logging
 import random
+import sys
 import time
 from argparse import ArgumentParser
 
 import requests
 
+logger = logging.getLogger(__name__)
 inputs = [
     "Hello, how are you?",
     "What up dig dog?",
@@ -39,9 +42,12 @@ def benchmark(*urls):
             res = requests.get(f"{isvc}/predict/{random.choice(inputs)}")
             if res.status_code == 200:
                 times.append(time.time() - start)
+                logger.info(f"Good status code from {isvc}: {res.status_code}")
                 count_s += 1
             else:
-                print(f"Bad status code from {isvc}: {res.status_code}")
+                logger.warning(
+                    f"Bad status code from {isvc}: {res.status_code}"
+                )
                 count_f += 1
 
         print(f"Average Latency for {isvc}: {sum(times)/len(times)}")
@@ -52,14 +58,36 @@ def benchmark(*urls):
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--url", help="InferenceService URL", required=True)
+    parser.set_defaults(log_level=logging.WARNING)
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Show more logs",
+        dest="log_level",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "--quiet",
+        "-q",
+        help="Show fewer logs",
+        dest="log_level",
+        action="store_const",
+        const=logging.ERROR,
+    )
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    return args
+
+def setup_logger(level):
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.propagate = False
+    logger.setLevel(level)
 
 
 def main():
     args = parse_args()
+    setup_logger(args.log_level)
     benchmark(args.url)
 
 
