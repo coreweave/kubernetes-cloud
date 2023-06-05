bab12d5d1f9f3897913a64685d01078bbdf3d075
Eta <esyra@coreweave.com>
2023-06-05 17:50:05 -0500

    refactor(tensorizer-isvc): Improve docstring and errors for `load_model`

--- online-inference/tensorizer-isvc/tensorizer_hf_isvc/load_model.py
+++ online-inference/tensorizer-isvc/tensorizer_hf_isvc/load_model.py
@@ -1,33 +1,41 @@
 import time
+from typing import Literal
 
 import torch
 from tensorizer import TensorDeserializer
 from tensorizer.utils import convert_bytes, get_mem_usage, no_init_or_tensor
 from transformers import AutoConfig, AutoModelForCausalLM, GPTJForCausalLM
 
-# GLOBALS
-
 DEVICE = "cuda"
-
-"""
-Loads the model using Tensorizer or Huggingface.
-
-params:
-- model_load_type (str): Type of model load [Options: "tensorizer", "hf"]
-"""
 
 
 def load_model_based_on_type(
-    model_load_type: str = "tensorizer", model_path: str = "/mnt/pvc"
+    model_load_type: Literal["tensorizer", "hf"] = "tensorizer",
+    model_path: str = "/mnt/pvc",
 ):
-    assert model_load_type == "hf" or model_load_type == "tensorizer"
+    """
+    Loads the model using Tensorizer or HuggingFace.
+
+    Args:
+        model_load_type: Method to load the model [Options: "tensorizer", "hf"]
+        model_path: Path to the model files
+    """
+    if model_load_type not in ("tensorizer", "hf"):
+        raise ValueError(
+            'model_load_type must be either "tensorizer" or "hf";'
+            f" got {model_load_type}"
+        )
 
     if model_load_type == "hf":
         start = time.time()
         model = GPTJForCausalLM.from_pretrained(
             model_path, torch_dtype=torch.float16
         ).to(DEVICE)
-        print(f"Start time for model load from HF: {time.time() - start} seconds")
+        duration = time.time() - start
+        print(
+            f"Deserialized model in {duration:0.2f}s"
+            " using HuggingFace Transformers"
+        )
 
         return model
 
@@ -42,7 +50,9 @@ def load_model_based_on_type(
 
     # Lazy load the tensors from PVC into the model.
     start = time.time()
-    deserializer = TensorDeserializer(f"{model_path}/gptj.tensors", plaid_mode=True)
+    deserializer = TensorDeserializer(
+        f"{model_path}/gptj.tensors", plaid_mode=True
+    )
     deserializer.load_into_module(model)
     end = time.time()
 
@@ -52,7 +62,10 @@ def load_model_based_on_type(
     per_second = convert_bytes(deserializer.total_tensor_bytes / duration)
     after_mem = get_mem_usage()
     deserializer.close()
-    print(f"Deserialized {total_bytes_str} in {end - start:0.2f}s," f" {per_second}/s")
+    print(
+        f"Deserialized {total_bytes_str} in {duration:0.2f}s, {per_second}/s"
+        " using Tensorizer"
+    )
     print(f"Memory usage before: {before_mem}")
     print(f"Memory usage after: {after_mem}")
 
