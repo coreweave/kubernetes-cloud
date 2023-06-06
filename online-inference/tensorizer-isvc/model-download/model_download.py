9ca08f96eb04f1bc9671443ec27db7e530f4f053
Eta <esyra@coreweave.com>
2023-06-05 22:52:19 -0500

    fix(tensorizer-isvc): Fit model download job within RAM constraints

--- online-inference/tensorizer-isvc/model-download/model_download.py
+++ online-inference/tensorizer-isvc/model-download/model_download.py
@@ -4,18 +4,22 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_ref = "EleutherAI/gpt-j-6b"
 
+tokenizer = AutoTokenizer.from_pretrained(model_ref)
+tokenizer.save_pretrained("/mnt")
+del tokenizer
+
 model = AutoModelForCausalLM.from_pretrained(
     model_ref,
     revision="float16",
     torch_dtype=torch.float16,
     low_cpu_mem_usage=True,
 )
-
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+# If only the tensorized model is desired, instead of saving the whole
+# PyTorch model, only the PretrainedConfig for the model need be saved
+# with the tokenizer and .tensors file.
+# model.config.save_pretrained("/mnt")
+model.save_pretrained("/mnt")
 
 serializer = TensorSerializer("/mnt/gptj.tensors")
-serializer.write_module(model)
+serializer.write_module(model, remove_tensors=True)
 serializer.close()
-
-model.save_pretrained("/mnt")
-tokenizer.save_pretrained("/mnt")
