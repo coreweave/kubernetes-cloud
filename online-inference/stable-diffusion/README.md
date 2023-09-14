eed9e1d97def3f15810a175623395af23c904ceb
Anthony Mercurio <amercurio@coreweave.com>
2023-09-13 17:20:05 -0700

    feat(sd-inference): add inference overhaul

--- online-inference/stable-diffusion/README.md
+++ online-inference/stable-diffusion/README.md
@@ -1,8 +1,2 @@
 # Stable Diffusion
 Please refer to [CoreWeave Docs](https://docs.coreweave.com/machine-learning-and-ai/inference/examples/pytorch-jax/hugging-face/pytorch-hugging-face-diffusers-stable-diffusion-text-to-image) for a deployment tutorial. 
-
-## Example:
-![stable diffusion, digital art, trending, HD](stable-diffusion.png)
-```
-{"prompt": "stable diffusion, digital art, trending, HD", "parameters": {"seed": 42424242}}
-```
--- online-inference/stable-diffusion/service/service.py
+++ online-inference/stable-diffusion/service/service.py
@@ -1,274 +1,146 @@
-import collections
-import json
-import tempfile
-import time
-
-import kserve
-import logging
 import os
-from typing import Dict, Union, Optional
-from argparse import ArgumentParser, Namespace
-from io import BytesIO
+import io
+import logging
+import time
+from typing import Optional
+from argparse import ArgumentParser
 
-import tensorizer
-
+import uvicorn
 import torch
-from torch import autocast
+import tensorizer
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from transformers import CLIPTextModel, CLIPTextConfig, CLIPTokenizer
 from diffusers import (
     StableDiffusionPipeline,
     LMSDiscreteScheduler,
     AutoencoderKL,
-    UNet2DConditionModel, ModelMixin, ConfigMixin,
+    UNet2DConditionModel
 )
-from transformers import CLIPTextModel, CLIPTextConfig, CLIPTokenizer, AutoConfig, PreTrainedModel
 
-HF_TOKEN = os.getenv("HUGGING_FACE_HUB_TOKEN")
+# Logger setup
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__file__)
 
-logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
-logger = logging.getLogger()
+# Argument parser setup
+parser = ArgumentParser()
+parser.add_argument("--model-uri", default=os.getenv("MODEL_URI", "s3://tensorized/runwayml/stable-diffusion-v1-5"), type=str)
+parser.add_argument("--precision", choices=["float16", "float32"], default="float16", type=str)
+parser.add_argument("--device-id", default=0, help="GPU ID to use for inference, or -1 for CPU [default = 0]")
+parser.add_argument("--port", default=8080, help="Port to listen on [default = 80 (http)]", type=int)
+parser.add_argument("--ip", type=str, default="0.0.0.0", help="IP address to listen on [default = 0.0.0.0 (all interfaces)]")
+parser.add_argument("--s3-access-key", default=os.getenv("AWS_KEY"), type=str)
+parser.add_argument("--s3-secret-access-key", default=os.getenv("AWS_SECRET"), type=str)
+parser.add_argument("--s3-endpoint", default=os.getenv("AWS_HOST", "accel-object.ord1.coreweave.com"), type=str)
+args = parser.parse_args()
 
-
-def get_args() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--model-id", default=os.getenv("MODEL_ID", "/mnt/models/CompVis/stable-diffusion-v1-4"), type=str,
+def load_artifact(path_uri, module) -> None:
+    deserializer = tensorizer.TensorDeserializer(
+        file_obj=tensorizer.stream_io.open_stream(
+            path_uri=path_uri,
+            mode="rb",
+            s3_access_key_id = args.s3_access_key,
+            s3_secret_access_key = args.s3_secret_access_key,
+            s3_endpoint = args.s3_endpoint,
+            s3_config_path=None,
+        ),
+        plaid_mode=True
     )
-    parser.add_argument(
-        "--precision", choices=["float16", "float32"], default="float16", type=str
-    )
-    parser.add_argument("--guidance-scale", default=float(os.getenv("CONDITION_SCALE", 7.0)), type=float)
-    parser.add_argument("--num-inference-steps", default=int(os.getenv("NUM_INFERENCE_STEPS", 50)), type=int)
-    parser.add_argument("--seed", default=os.getenv("SEED"), type=int)
-    parser.add_argument("--width", default=int(os.getenv("WIDTH", 512)), type=int)
-    parser.add_argument("--height", default=int(os.getenv("HEIGHT", 512)), type=int)
-    parser.add_argument("--tensorized", default=False, action="store_true")
-    args = parser.parse_args()
+    deserializer.load_into_module(module)
+    deserializer.close()
+    logger.info(f"Loaded tensorized S3 artifact from {path_uri}")
 
-    args.model_name = args.model_id.split("/")[-1]
+def load_model_s3(path_uri) -> StableDiffusionPipeline:
+    logger.info(f"Loading {args.precision} model from S3 URI: {path_uri}")
+    hf_id = '/'.join(path_uri.split("/")[-2:])
+    text_encoder_config = CLIPTextConfig.from_pretrained(hf_id, subfolder="text_encoder")
 
-    return args
+    with tensorizer.utils.no_init_or_tensor():
+        unet = UNet2DConditionModel.from_config(UNet2DConditionModel.load_config(hf_id, subfolder="unet"))
+        vae = AutoencoderKL.from_config(AutoencoderKL.load_config(hf_id, subfolder="vae"))
+        text_encoder = CLIPTextModel(text_encoder_config)
 
+    dtype_str = "/fp16" if args.precision == "float16" else ""
 
-def set_logger(name: str):
-    global logger
-    logger = logging.getLogger(name)
+    start = time.time()
 
+    load_artifact(f"{path_uri}{dtype_str}/unet.tensors", unet)
+    load_artifact(f"{path_uri}{dtype_str}/vae.tensors", vae)
+    load_artifact(f"{path_uri}{dtype_str}/text_encoder.tensors", text_encoder)
 
-def load_tensorizer_model(
-    path_uri: str,
-    modelclass: Union[PreTrainedModel, ModelMixin, ConfigMixin] = None,
-    configclass: Optional[Union[ConfigMixin, AutoConfig]] = None,
-    model_prefix: str = "model",
-    device: torch.device = tensorizer.utils.get_device(),
-    dtype: str = None
-) -> torch.nn.Module:
-    """
-    Given a path prefix, load the model with a custom extension
-    Args:
-        path_uri: path to the model. Can be a local path or a URI
-        modelclass: The model class to load the tensors into.
-        configclass: The config class to load the model config into. This must be
-            set if you are loading a model from HuggingFace Transformers.
-        model_prefix: The prefix to use to distinguish between multiple serialized
-            models. The default is "model".
-        device: The device to load the tensors to.
-        dtype: The dtype to load the tensors into. If None, the dtype is inferred from
-            the model.
-    """
+    logger.info(f"All artifacts loaded successfully in {time.time() - start:.2f} seconds")
 
-    if model_prefix is None:
-        model_prefix = "model"
-
-    begin_load = time.time()
-    ram_usage = tensorizer.utils.get_mem_usage()
-
-    config_uri = f"{path_uri}/{model_prefix}-config.json"
-    tensors_uri = f"{path_uri}/{model_prefix}.tensors"
-    tensor_stream = tensorizer.stream_io.open_stream(tensors_uri)
-
-    logger.info(f"Loading {tensors_uri}, {ram_usage}")
-
-    tensor_deserializer = tensorizer.TensorDeserializer(
-        tensor_stream, device=device, dtype=dtype, lazy_load=True
+    return StableDiffusionPipeline(
+        vae=vae,
+        text_encoder=text_encoder,
+        unet=unet,
+        tokenizer=CLIPTokenizer.from_pretrained(hf_id, subfolder="tokenizer"),
+        scheduler=LMSDiscreteScheduler.from_pretrained(hf_id, subfolder="scheduler"),
+        safety_checker=None,
+        feature_extractor=None,
+        requires_safety_checker=False
     )
 
-    if configclass is not None:
-        try:
-            with tempfile.TemporaryDirectory() as dir:
-                open(os.path.join(dir, "config.json"), "w").write(
-                    tensorizer.stream_io.open_stream(config_uri).read().decode("utf-8")
-                )
-                config = configclass.from_pretrained(dir)
-                config.gradient_checkpointing = True
-        except ValueError:
-            config = configclass.from_pretrained(config_uri)
-        model = tensorizer.utils.no_init_or_tensor(
-            lambda: modelclass.from_pretrained(
-                None, config=config, state_dict=collections.OrderedDict()
-            )
-        )
+def load_model_local(path_uri) -> StableDiffusionPipeline:
+    logger.info(f"Loading local model from URI: {path_uri}")
+    return StableDiffusionPipeline.from_pretrained(path_uri, dtype=getattr(torch, args.precision))
+
+def load_model(path_uri) -> StableDiffusionPipeline:
+    if path_uri.startswith("s3://"):
+        return load_model_s3(path_uri)
     else:
-        try:
-            config = json.loads(
-                tensorizer.stream_io.open_stream(config_uri).read().decode("utf-8")
-            )
-        except ValueError:
-            with open(config_uri, "r") as f:
-                config = json.load(f)
-        model = tensorizer.utils.no_init_or_tensor(lambda: modelclass(**config))
+        return load_model_local(path_uri)
 
-    tensor_deserializer.load_into_module(model)
+class Generation(BaseModel):
+    prompt: str
+    guidance_scale: Optional[float] = 7.0
+    num_inference_steps: Optional[int] = 28
+    seed: Optional[int] = None
+    width: Optional[int] = 512
+    height: Optional[int] = 512
 
-    tensor_load_s = time.time() - begin_load
-    rate_str = tensorizer.utils.convert_bytes(
-        tensor_deserializer.total_bytes_read / tensor_load_s
-    )
-    tensors_sz = tensorizer.utils.convert_bytes(tensor_deserializer.total_bytes_read)
-    logger.info(
-        f"Model tensors loaded in {tensor_load_s:0.2f}s, read "
-        + f"{tensors_sz} @ {rate_str}/s, {tensorizer.utils.get_mem_usage()}"
-    )
+app = FastAPI(title="Stable Diffusion Inference API")
+logger.info(f"Setting CUDA device ID: {args.device_id}")
+torch.cuda.set_device(args.device_id)
+pipe = load_model(args.model_uri).to('cuda')
+pipe.set_progress_bar_config(disable=True)
 
-    return model
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
+@app.get("/")
+def get_health():
+    logger.info("Health check requested")
+    return "OK"
 
-class Model(kserve.Model):
-    def __init__(self,
-                 model_name: str,
-                 model_id: str,
-                 precision: str,
-                 guidance_scale: float,
-                 num_inference_steps: int,
-                 width: int,
-                 height: int,
-                 tensorized: bool,
-                 seed: int):
-        super().__init__(model_name)
-
-        self.ready = False
-        self.pipeline = None
-        self.model_name = model_name
-        self.model_id = model_id
-        self.precision = precision
-        self.tensorized = tensorized
-
-        self.parameters = {
-            "GUIDANCE_SCALE": guidance_scale,
-            "NUM_INFERENCE_STEPS": num_inference_steps,
-            "SEED": seed,
-            "WIDTH": width,
-            "HEIGHT": height,
-        }
-
-    def load_diffusers(self):
-        self.pipeline = StableDiffusionPipeline.from_pretrained(
-            self.model_id,
-            torch_dtype=getattr(torch, self.precision),
-            scheduler=LMSDiscreteScheduler.from_pretrained(
-                self.model_id, subfolder="scheduler"
-            ),
-            local_files_only=True,
-        )
-
-    def load_tensorizer(self):
-        vae = load_tensorizer_model(self.model_id, AutoencoderKL, None, "vae")
-        unet = load_tensorizer_model(
-            self.model_id, UNet2DConditionModel, None, "unet"
-        )
-        encoder = load_tensorizer_model(
-            self.model_id, CLIPTextModel, CLIPTextConfig, "encoder"
-        )
-
-        scheduler = LMSDiscreteScheduler.from_pretrained(
-            self.model_id, subfolder="scheduler", use_auth_token=HF_TOKEN
-        )
-
-        tokenizer = CLIPTokenizer.from_pretrained(
-            self.model_id, subfolder="tokenizer", use_auth_token=HF_TOKEN
-        )
-
-        self.pipeline = StableDiffusionPipeline(
-            text_encoder=encoder,
-            vae=vae,
-            unet=unet,
-            scheduler=scheduler,
-            tokenizer=tokenizer,
-            safety_checker=None,
-            feature_extractor=None,
-        )
-
-    def load(self):
-        logger.info(f"Loading {self.model_name}")
-
-        if self.tensorized:
-            self.load_tensorizer()
-        else:
-            self.load_diffusers()
-
-        logger.info(f"Loaded {self.model_name}")
-
-        logger.info(f"Loading {self.model_name} to accelerator")
-        self.pipeline.to("cuda")
-        logger.info(f"Accelerator loaded")
-
-        self.ready = True
-
-    def configure_request(self, request: Dict, request_parameters) -> Dict:
-        parameters = request["parameters"]
-        for k, pv in parameters.items():
-            pk = k.upper()
-            if pk in request_parameters:
-                request_parameters[pk] = pv
-                logger.debug(
-                    f"Parameter {pk} changed from {request_parameters[pk]} to {pv}"
-                )
-
-        return request_parameters
-
-    def predict(self, request: Dict) -> Dict:
-        request_parameters = self.parameters.copy()
-        if "parameters" in request:
-            logger.debug(f"Configuring request")
-            request_parameters = self.configure_request(
-                request, request_parameters
-            )
-            logger.debug(f"Request configured")
-
+@app.post("/generate")
+def generate(generation: Generation):
+    try:
         generator = None
-        if request_parameters["SEED"] is not None:
-            generator = torch.Generator("cuda").manual_seed(
-                request_parameters["SEED"]
-            )
+        image = io.BytesIO()
 
-        logger.debug(f"Generating image")
-        with autocast("cuda"):
-            image = self.pipeline(
-                request["prompt"],
-                height=request_parameters["HEIGHT"],
-                width=request_parameters["WIDTH"],
-                guidance_scale=request_parameters["GUIDANCE_SCALE"],
-                num_inference_steps=request_parameters["NUM_INFERENCE_STEPS"],
-                generator=generator,
-            ).images[0]
-        logger.debug(f"Image generated")
+        if generation.seed is not None:
+            generator = torch.Generator(device='cuda').manual_seed(generation.seed)
 
-        image_file = BytesIO()
-        image.save(image_file, format="PNG")
-        response = image_file.getvalue()
+        pipe(
+            prompt=generation.prompt,
+            height=generation.height,
+            width=generation.width,
+            num_inference_steps=generation.num_inference_steps,
+            guidance_scale=generation.guidance_scale,
+            generator=generator,
+            output_type="pil"
+        ).images[0].save(image, format='PNG')
 
-        return response
+        return Response(content=image.getvalue(), media_type="image/png")
+    except Exception as e:
+        return Response(content=str(e), media_type="text/plain", status_code=500)
 
-
-def main():
-    args = get_args()
-    set_logger(args.model_name)
-    logger.info(f"Model Name: {args.model_name}")
-    logger.info(f'Model ID: {args.model_id}')
-
-    model = Model(**vars(args))
-    model.load()
-    kserve.ModelServer().start([model])
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    logger.info(f"Starting inference server at {args.ip}:{args.port}")
+    uvicorn.run(app, host=args.ip, port=int(args.port))
